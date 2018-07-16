bd580d375ac5bf5a8a3f139f2e9091326e32b691
kongxingongzi <1543892875@qq.com>
2018-07-16 09:42:10 +0800

    Updated database configuration5

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,8 +2,8 @@
 
 function get_db_config()
 {
-    if (1/*getenv('IS_IN_HEROKU')*/) {
-        $url = parse_url('postgres://pxinsmnhndpzix:80ad5011fc85f1d8d2b44ffd56349976bbbaa8222d9868fdacffe5c022f0172a@ec2-23-21-164-107.compute-1.amazonaws.com:5432/dbiltqkmj09bnc');
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
             'connetion' => 'pgsql',
@@ -15,10 +15,10 @@ function get_db_config()
     } else {
         return $db_config = [
             'connetion' => env('DB_CONNECTION', 'mysql'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'database'  => env('DB_DATABASE', 'myblog'),
-            'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'root'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
         ];
     }
 }
